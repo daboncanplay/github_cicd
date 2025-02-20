7418ab021ed15e28f0cf06322991f1cd3e2d06cf
daboncanplay <daboncanplay@yahoo.com>
2025-02-20 16:53:19 -0700

    testing failed integration test

--- tests/integration.py
+++ tests/integration.py
@@ -1,8 +1,8 @@
 import requests
 
-url = 'https://cicd-example-456176258602.us-central1.run.app/predict'
+url = 'https://staging-example-456176258602.us-central1.run.app/predict'
 data = '''{"query":"This has nothing to do with anything, but there is a man named Harvey, and he just turned 40."}'''
 response = requests.post(url, data=data)
-assert response.json()["name"] == "Harvey"
+assert response.json()["name"] == "Harve"
 assert response.json()["age"] == 40
 print("All tests passed!")
