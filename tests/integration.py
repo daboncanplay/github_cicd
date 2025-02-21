ff786cd17526ae89f4bab1d8d991ac448ec9761c
daboncanplay <daboncanplay@yahoo.com>
2025-02-20 17:01:15 -0700

    oops now all tests should pass

--- tests/integration.py
+++ tests/integration.py
@@ -3,6 +3,6 @@ import requests
 url = 'https://staging-example-456176258602.us-central1.run.app/predict'
 data = '''{"query":"This has nothing to do with anything, but there is a man named Harvey, and he just turned 40."}'''
 response = requests.post(url, data=data)
-assert response.json()["name"] == "Harve"
+assert response.json()["name"] == "Harvey"
 assert response.json()["age"] == 40
 print("All tests passed!")
