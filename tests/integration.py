368a654b1d3096228d4b56ef76b29f8a308152af
daboncanplay <daboncanplay@yahoo.com>
2025-02-24 13:54:25 -0700

    my commit

--- tests/integration.py
+++ tests/integration.py
@@ -2,8 +2,7 @@ import requests
 
 url = 'https://staging-example-456176258602.us-central1.run.app/predict'
 
-data = '''{"query":"This has nothing to do with anything, 
-but there is a man named Harvey, and he just turned 40."}'''
+data = '''{"query":"This has nothing to do with anything, but there is a man named Harvey, and he just turned 40."}'''
 
 response = requests.post(url, data=data)
 
